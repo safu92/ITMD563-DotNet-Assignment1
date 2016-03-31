43d1e1eaf06e9784a6aec0f5c8b2679adc550570
Safdar <safu92@gmail.com>
2016-03-31 01:51:04 -0500

    Final Commit

---
+++ Http Module Assignment 1/App_Code/IPModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Http_Module_Assignment_1.App_Code
+{
+    public class IPModule : IHttpModule
+    {
+        public IPModule() { }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(Application_BeginRequest);
+        }
+
+        private void Application_BeginRequest(object source, EventArgs e)
+        {
+            HttpContext context = ((HttpApplication)source).Context;
+            string ipAddress = context.Request.UserHostAddress;
+            //context.Response.Write(ipAddress);
+            if (IsValidIpAddress(ipAddress))
+            {
+                context.Response.StatusCode = 403;  // (Forbidden)
+
+            }
+        }
+
+        private bool IsValidIpAddress(string ipAddress)
+        {
+            return (ipAddress == "192.168.1.90");
+        }
+
+        public void Dispose() { /* clean up */ }
+    }
+}
---
+++ Http Module Assignment 1/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Http_Module_Assignment_1.Global" Language="C#" %>
